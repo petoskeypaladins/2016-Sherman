34c01c4b58bc3365daf395892e3479698046bc40
Emily Dudd <dudderkinz123@gmail.com>
2016-02-17 17:33:00 -0500

    Added Shooter Rotate PID file (not completed)

---
+++ src/org/usfirst/frc/team3618/robot/subsystems/ShooterRotatePID.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team3618.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+
+/**
+ *
+ */
+public class ShooterRotatePID extends PIDSubsystem {
+
+    // Initialize your subsystem here
+    public ShooterRotatePID() {
+    	
+    	super("ShooterRotate", 2.0, 0.0, 0.0);
+        // Use these to get going:
+        // setSetpoint() -  Sets where the PID controller should move the system
+        //                  to
+        // enable() - Enables the PID controller.
+    }
+    
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    protected double returnPIDInput() {
+        // Return your input value for the PID loop
+        // e.g. a sensor, like a potentiometer:
+        // yourPot.getAverageVoltage() / kYourMaxVoltage;
+    	return 0.0;
+    }
+    
+    protected void usePIDOutput(double output) {
+        // Use output to drive your system, like a motor
+        // e.g. yourMotor.set(output);
+    }
+}
