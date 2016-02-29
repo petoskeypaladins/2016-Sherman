4f5be682620631ee5d8fbeb401a9fbdc553195d3
Stuart Green <stuart@terasmediaco.com>
2016-02-29 16:04:25 -0500

    Autonomous stsuff

---
+++ src/org/usfirst/frc/team3618/robot/commands/autonomous/EncoderDriveCommand.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team3618.robot.commands.autonomous;
+
+import org.usfirst.frc.team3618.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class EncoderDriveCommand extends Command {
+
+	double distance, power;
+	
+    public EncoderDriveCommand(double distance, double power) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.driveSubsystem);
+    	
+    	this.distance = distance;
+    	this.power = power;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.driveSubsystem.resetEncoders();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.driveSubsystem.AutonDrive(Robot.driveSubsystem.accel(power, timeSinceInitialized(), 0.5));
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return Robot.driveSubsystem.getEncoders() >= Robot.driveSubsystem.getTicksFromFeet(distance);
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.driveSubsystem.stopMe();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
